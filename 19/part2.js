6b5c436b93eb3265389409a1773b54c2d0763dbd
lchaumont <chaumontlouis718@gmail.com>
2023-12-26 08:49:16 +0100

    Day 19

--- 19/part2.js
+++ 19/part2.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 const main = (input) => {
     input = input
         .replaceAll("\r", "")
@@ -21,7 +23,7 @@ const main = (input) => {
             return {condition, direction};
         });
 
-        return {name, rules, usedTime : 0};
+        return {name, rules, usedTime: 0};
     });
 
     parts = parts.map((x) => {
@@ -36,119 +38,55 @@ const main = (input) => {
         return p;
     });
 
-    function getReturnForDirection(direction) {
-        if (direction === "A") return true;
-        else if (direction === "R") return false;
-        else return mapWorkflow.get(direction);
-    }
-
-    function executeWorkflow(workflow, part) {
-        const {x, m, a, s} = part;
-        const rules = workflow;
-
-        for (const rule of rules) {
-            if (rule.condition === null) return getReturnForDirection(rule.direction);
-
-            const variable = rule.condition[0];
-            const operator = rule.condition[1];
-            const value = rule.condition.substring(2);
-            if (variable === "x" && operator === "<" && x < value) return getReturnForDirection(rule.direction);
-            if (variable === "x" && operator === ">" && x > value) return getReturnForDirection(rule.direction);
-            if (variable === "m" && operator === "<" && m < value) return getReturnForDirection(rule.direction);
-            if (variable === "m" && operator === ">" && m > value) return getReturnForDirection(rule.direction);
-            if (variable === "a" && operator === "<" && a < value) return getReturnForDirection(rule.direction);
-            if (variable === "a" && operator === ">" && a > value) return getReturnForDirection(rule.direction);
-            if (variable === "s" && operator === "<" && s < value) return getReturnForDirection(rule.direction);
-            if (variable === "s" && operator === ">" && s > value) return getReturnForDirection(rule.direction);
-        }
-        
-        return false;
-    }
-
-    function collectDistinctRating(rating) {
-        return [
-            ...new Set(
-                workflows
-                    .map((x) => x.rules)
-                    .flat()
-                    .filter((x) => x.condition !== null && x.condition.includes(rating))
-                    .map((x) => x.condition.replaceAll(rating, ""))
-                    .map((x) => {
-                        const sign = x[0];
-                        return parseInt(x.substring(1));
-                    })
-            ),
-        ].sort((a, b) => a - b);
-    }
-
     const mapWorkflow = new Map();
     workflows.forEach((x) => mapWorkflow.set(x.name, x.rules));
     const inWorkflow = mapWorkflow.get("in");
+    const possibleIntervals = [];
 
-    const distinctA = collectDistinctRating("a");
-    const distinctM = collectDistinctRating("m");
-    const distinctS = collectDistinctRating("s");
-    const distinctX = collectDistinctRating("x");
-
-    distinctA.unshift(1);
-    distinctM.unshift(1);
-    distinctS.unshift(1);
-    distinctX.unshift(1);
-
-    distinctA.push(4000);
-    distinctM.push(4000);
-    distinctS.push(4000);
-    distinctX.push(4000);
-
-    console.log(distinctA);
-    console.log(distinctM);
-    console.log(distinctS);
-    console.log(distinctX);
-
-    let sum = 0;
-    for (let j = 0; j < distinctA.length -1 ; j++) {
-        const a = distinctA[j];
-        const nextA = distinctA[j + 1];
-        const fA = nextA - a;
-
-        console.log("-------- ", j);
-
-        for (let h = 0; h < distinctM.length - 1; h++) {
-            const m = distinctM[h];
-            const nextM = distinctM[h + 1];
-            const fM = nextM - m;
-
-            console.log("------ ", h);
-
-            for (let g = 0; g < distinctS.length - 1; g++) {
-                const s = distinctS[g];
-                const nextS = distinctS[g + 1];
-                const fS = nextS - s;
-
-                for (let i = 0; i < distinctX.length - 1; i++) {
-                    const x = distinctX[i];
-
-                    const part = {a: a + 0.5, m: m + 0.5, s: s + 0.5, x: x + 0.5};
-                    let safeLoop = 0;
-                    let currentWorkflow = inWorkflow;
-
-                    while (currentWorkflow !== true && currentWorkflow !== false && safeLoop < 560) {
-                        currentWorkflow = executeWorkflow(currentWorkflow, part);
-                        safeLoop++;
-                    }
-
-                    if (currentWorkflow === true) {
-                        const nextX = distinctX[i + 1];
-                        const fX = nextX - x;
-                        
-                        const factor = xLength * sLength * mLength * aLength;
-                        sum = sum + factor;
-                    }
-                }
-            }
+    function executeWorkflow(rules, pool) {
+        if (rules.length === 1) {
+            if (rules[0].direction === "A") {
+                possibleIntervals.push(_.cloneDeep(pool));
+                return;
+            };
+            if (rules[0].direction === "R") return;
+            executeWorkflow(mapWorkflow.get(rules[0].direction), pool);
+        } else {
+            executeWorkflow([rules[0]], splitPool(pool, rules[0].condition));
+            executeWorkflow(rules.slice(1), splitPool(pool, rules[0].condition, true));
         }
     }
-    console.log(sum);
+
+    function splitPool(pool, condition, reversed = false) {
+        const p = _.cloneDeep(pool);
+        const variable = condition[0]; // a, m, s, x
+        const operator = condition[1]; // <, >
+        const value = parseInt(condition.slice(2));
+
+        const interval = p[variable];
+        if (!interval) return;
+
+        if (reversed) {
+            if (operator === ">") interval[1] = Math.min(interval[1], value);
+            if (operator === "<") interval[0] = Math.max(interval[0], value);
+        } else {
+            if (operator === ">") interval[0] = Math.max(interval[0], value + 1);
+            if (operator === "<") interval[1] = Math.min(interval[1], value - 1);
+        }
+
+        if (interval[0] > interval[1]) return;
+        p[variable] = interval;
+        return p;
+    }
+
+    executeWorkflow(inWorkflow, {x: [1, 4000], m: [1, 4000], a: [1, 4000], s: [1, 4000]});
+    console.log(possibleIntervals);
+
+    const result = possibleIntervals.map((interval) => {
+        return (interval.x[1] - interval.x[0] + 1) * (interval.m[1] - interval.m[0] + 1) * (interval.a[1] - interval.a[0] + 1) * (interval.s[1] - interval.s[0] + 1); 
+    }).reduce((a, b) => a + b, 0);
+
+    console.log(result);
 };
 
 export default main;
