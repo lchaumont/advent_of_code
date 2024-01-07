670db13f6417246e820ab1a50c303fda7d5da9a4
lchaumont <chaumontlouis718@gmail.com>
2024-01-07 15:09:30 +0100

    2021 - Day 2

--- 2021/2/part1.ts
+++ 2021/2/part1.ts
@@ -3,6 +3,24 @@ const main = (input: string) => {
         .replaceAll("\r", "")
         .split("\n")
         .filter((line: string) => line.trim() !== "");
+
+    const pos = {x: 0, y: 0};
+
+    lines.forEach((line: string) => {
+        let [dir, distString] = line.split(" ");
+        const dist = parseInt(distString);
+
+        if (dir === "forward") {
+            pos.x = pos.x + dist;
+        } else if (dir === "down") {
+            pos.y = pos.y + dist;
+        } else if (dir === "up") {
+            pos.y = pos.y - dist;
+        }
+    });
+
+    console.log(pos);
+    console.log(pos.x * pos.y);
 };
 
 export default main;
--- 2021/2/part2.ts
+++ 2021/2/part2.ts
@@ -3,6 +3,25 @@ const main = (input: string) => {
         .replaceAll("\r", "")
         .split("\n")
         .filter((line: string) => line.trim() !== "");
+
+    const pos = {x: 0, y: 0, aim: 0};
+
+    lines.forEach((line: string) => {
+        let [dir, distString] = line.split(" ");
+        const dist = parseInt(distString);
+
+        if (dir === "forward") {
+            pos.x = pos.x + dist;
+            pos.y = pos.y + pos.aim * dist;
+        } else if (dir === "down") {
+            pos.aim = pos.aim + dist;
+        } else if (dir === "up") {
+            pos.aim = pos.aim - dist;
+        }
+    });
+
+    console.log(pos);
+    console.log(pos.x * pos.y);
 };
 
 export default main;
