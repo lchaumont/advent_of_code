77a5ee683ecb2d1ae81bd2b758cadddd99e38a45
lchaumont <louis.chaumont@antenia.fr>
2024-01-08 13:18:32 +0100

    2020 - Day 1

---
+++ 2020/1/part1.py
@@ -0,0 +1,9 @@
+with open("input.txt", 'r') as f:
+    lines = f.read().splitlines()
+    lines = list(map(lambda x: int(x), lines))
+    
+    for i in range(len(lines)):
+        for j in range(i, len(lines)):
+            if lines[i] + lines[j] == 2020:
+                print(lines[i] * lines[j])
+                break
