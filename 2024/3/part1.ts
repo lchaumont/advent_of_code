9d198adc72a0bc6cceec582b322034c9acfa8a93
lchaumont <chaumontlouis718@gmail.com>
2024-12-03 06:49:17 +0100

    2024 - 3

--- 2024/3/part1.ts
+++ 2024/3/part1.ts
@@ -3,6 +3,21 @@ const main = (input: string) => {
         .replaceAll("\r", "")
         .split("\n")
         .filter((line: string) => line.trim() !== "");
+    
+    const regex = /mul\((\d+),(\d+)\)/g;
+
+    let result = 0;
+
+    lines.forEach((line: string) => {
+        [...line.matchAll(regex)].forEach((match) => {
+            const l = parseInt(match[1]);
+            const r = parseInt(match[2]);
+    
+            result += l * r;
+        });
+    });
+
+    return result;
 };
 
 export default main;
--- 2024/3/part2.ts
+++ 2024/3/part2.ts
@@ -3,6 +3,35 @@ const main = (input: string) => {
         .replaceAll("\r", "")
         .split("\n")
         .filter((line: string) => line.trim() !== "");
+    
+    const regex = /(?:mul\((\d+),(\d+)\))|do(?:n't)?\(\)/g;
+
+    let result = 0;
+
+    let enabled: boolean = true;
+
+    lines.forEach((line: string) => {
+        [...line.matchAll(regex)].forEach((match) => {
+            if (match[0].startsWith("don't")) {
+                enabled = false;
+                return;
+            }
+
+            if (match[0].startsWith("do")) {
+                enabled = true;
+                return;
+            }
+
+            if (!enabled) return;
+
+            const l = parseInt(match[1]);
+            const r = parseInt(match[2]);
+    
+            result += l * r;
+        });
+    });
+
+    return result;
 };
 
 export default main;
