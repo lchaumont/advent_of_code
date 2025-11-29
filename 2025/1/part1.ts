f35a73eb42e1f31877f845b5697b4a09cd31b716
lchaumont <chaumontlouis718@gmail.com>
2025-11-29 18:35:53 +0100

    Prepare for 2025...

---
+++ 2025/1/part1.ts
@@ -0,0 +1,8 @@
+const main = (input: string) => {
+    const lines: string[] = input
+        .replaceAll("\r", "")
+        .split("\n")
+        .filter((line: string) => line.trim() !== "");
+};
+
+export default main;
