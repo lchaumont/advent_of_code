93378199cb825f8901c498f2f3a36935b8336ae8
lchaumont <louis.chaumont@antenia.fr>
2025-12-01 13:07:54 +0100

    2025 - Day 1

--- 2025/1/part1.ts
+++ 2025/1/part1.ts
@@ -3,6 +3,20 @@ const main = (input: string) => {
         .replaceAll("\r", "")
         .split("\n")
         .filter((line: string) => line.trim() !== "");
+
+    let dial = 50;
+    let password = 0;
+
+    for (const line of lines) {
+        const sens = line[0];
+        const number = parseInt(line.substring(1));
+
+        dial += ((sens === "L" ? -1 : 1) * number);
+        if (dial % 100 === 0) password++;
+    }
+
+    console.log("Dial :", dial);
+    return password;
 };
 
 export default main;
--- 2025/1/part2.ts
+++ 2025/1/part2.ts
@@ -3,6 +3,28 @@ const main = (input: string) => {
         .replaceAll("\r", "")
         .split("\n")
         .filter((line: string) => line.trim() !== "");
+
+    let dial = 50;
+    let password = 0;
+
+    for (const line of lines) {
+        const sens = line[0];
+        const number = parseInt(line.substring(1));
+
+        const after = dial + (sens === "L" ? -1 : 1) * number;
+
+        const s1 = Math.floor(dial / 100);
+        const s2 = Math.floor(after / 100);
+
+        password += Math.abs(s1 - s2);
+        if (after % 100 === 0 && sens === "L") password++;
+        if (dial % 100 === 0 && sens === "L") password--;
+
+        dial = after;
+    }
+
+    console.log("Dial :", dial);
+    return password;
 };
 
 export default main;
